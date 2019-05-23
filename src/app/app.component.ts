4b2ca4e7b23d049c7c79d9fc6095394d9f3a2f27
victor.magalhaes <victor.magalhaes@nexxera.com>
2019-05-23 10:28:33 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  title = 'coffee-randomizer';
+  loadingSize = '0%';
+  choosen: String;
+  people: String[] = [
+    "Victor",
+    "Jéssica",
+    "João",
+    "Edinilton",
+    "Maurício",
+    "Akira",
+    "Nilson"
+  ];
+
+  ngOnInit() {
+    let elem = document.getElementById("progress-bar");
+    let width = 1;
+    let frame = () => {
+      if (width >= 100) {
+        clearInterval(id);
+        this.choosen = this.people[Math.floor(Math.random() * this.people.length)];
+      } else {
+        width++;
+        elem.style.width = width + '%';
+        this.loadingSize = width + '%';
+      }
+    }
+
+    let id = setInterval(frame, 30);
+  }
+}
